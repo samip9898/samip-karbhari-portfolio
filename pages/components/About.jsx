d9c5f623de88a30c57ac3f65ce2b1ff8930e3fe3
Samip <samipkarbhari.sk@gmail.com>
2022-11-13 18:49:32 -0500

    Commit About

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -15,27 +15,18 @@ const About = () => {
           <h2 className="py-4">Who I am</h2>
 
           <p className="py-2 text-gray-600">
-            I hold a Master&apos;s degree in Computer Science from Lakehead
-            University. I have around 3 years of work experience as a front-end
-            developer. During these years I have worked with React, HTML, CSS,
-            and also a little bit of Angular, and in terms of backend I have
-            worked with Node and Express.JS. My role as a front-end developer is
-            to produce clean and efficient code, based on specifications while
-            collaborating with the development team, gathering product
-            requirements from the client and stakeholders. I specialize in
-            developing ReactJS web applications by creating custom reusable
-            components for data manipulations and displaying data in company
-            standard format.
+            A Web Developer, passionate about solving problems and developing
+            web solutions, always eager to explore-learn-grab new skills and
+            technologies for remaining upgraded to work in collaborative
+            environments. A worthy team player with strong communication skills
+            and willingness to help. I am a Master&apos;s graduate having around
+            3 years of experience as a Front-End Developer primarily working
+            with JavaScript, React, HTML, Git for version control, REST APIs,
+            front-end designing and more. I specialize in developing ReactJS web
+            applications by creating custom reusable components for data
+            manipulations and displaying data in company standard format.
           </p>
-          <p className="py-2 text-gray-600">
-            I am a very energetic guy and a great communicator, and my work
-            experience in the domain of web development has helped me to build
-            confidence and taught me the importance of teamwork. I am very
-            punctual and dependable and can be counted upon to finish what I
-            start. I consider myself as a very hardworking guy and I like to
-            build a positive atmosphere around which also leads to a more
-            productive work.{" "}
-          </p>
+
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
